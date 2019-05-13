789e51861c80c0f7f39ac976080604d1f830283e
Anas I. Gouda <anas.ibrahim006@gmail.com>
2019-05-13 16:08:02 +0200

    added ping pong app in one project

---
+++ apps/ping_pong/flash_rx.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+make distclean
+make
+~/ti/uniflash_4.6.0/dslite.sh --config=targetConfigs/CC1350F128.ccxml pingPongRx.simplelink -O PinReset
+# sleep 1 
+# screen /dev/ttyACM0 115200

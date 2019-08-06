f701beba936ffb746eebca42a03810f639e4e9c1
Anas Gouda <anas.ibrahim006@gmail.com>
2019-08-06 11:25:03 +0200

    added app for to help associate 1-wire addresses to each sensortag (15) on 1 strip

---
+++ apps/address_finder/project-conf.h
@@ -0,0 +1,10 @@
+#ifndef PROJECT_CONF_H_
+#define PROJECT_CONF_H_
+
+#define LOG_LEVEL_APP LOG_LEVEL_INFO
+
+#define BOARD_ID 1
+#define PRINT_RATE 30
+#define LED_RATE 2
+
+#endif /* PROJECT_CONF_H_ */

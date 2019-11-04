1f6db2140ab47f30da2caee98e423af2801b8b8c
Anas Gouda <anas.ibrahim006@gmail.com>
2019-11-04 14:32:05 +0100

    glossy start implemented

--- apps/glossy_scratch/glossy.c
+++ apps/glossy_scratch/glossy.c
@@ -26,33 +26,36 @@
 // RF
 static RF_Object rfObject;
 static RF_Handle rfHandle;
-static RF_Handle rfHandle;
 static RF_Params rfParams;
 
 static uint8_t payload_with_counter[GLOSSY_PAYLOAD_LEN_WITH_COUNT];
 
-///* Buffer which contains all Data Entries for receiving data.
-// * Pragmas are needed to make sure this buffer is 4 byte aligned (requirement from the RF Core) */
-//static uint8_t
-//rxDataEntryBuffer[RF_QUEUE_DATA_ENTRY_BUFFER_SIZE(NUM_DATA_ENTRIES,
-//                                                  MAX_LENGTH,
-//                                                  NUM_APPENDED_BYTES)]
-//                                                  __attribute__((aligned(4)));
-//
-///* Receive dataQueue for RF Core to fill in data */
-//static dataQueue_t dataQueue;
-//static rfc_dataEntryGeneral_t* currentDataEntry;
-//static uint8_t packetLength;
-//static uint8_t* packetDataPointer;
-//static uint32_t rxTimestamp;
-//static uint32_t currentTimestamp;
+static uint8_t n_tx_count;
+static uint32_t initiator_cmd_base_time;
+
+/* Buffer which contains all Data Entries for receiving data.
+ * Pragmas are needed to make sure this buffer is 4 byte aligned (requirement from the RF Core) */
+static uint8_t
+rxDataEntryBuffer[RF_QUEUE_DATA_ENTRY_BUFFER_SIZE(NUM_DATA_ENTRIES,
+                                                  MAX_LENGTH,
+                                                  NUM_APPENDED_BYTES)]
+                                                  __attribute__((aligned(4)));
+
+/* Receive dataQueue for RF Core to fill in data */
+static dataQueue_t dataQueue;
+static rfc_dataEntryGeneral_t* currentDataEntry;
+static uint8_t packetLength;
+static uint8_t* packetDataPointer;
+static uint32_t rxTimestamp;
+static uint32_t currentTimestamp; //TODO delete
 
 //static uint8_t packet[MAX_LENGTH + NUM_APPENDED_BYTES - 1]; /* The length byte is stored in a separate variable */
 
-//static bool tx_callback_called = false; //TODO delete
+static bool tx_callback_called = false; //TODO delete
+static bool rx_callback_called = false; //TODO delete
 
 /*---------------------------------------------------------------------------*/
-//void fs_callback(RF_Handle h, RF_CmdHandle ch, RF_EventMask e)
+//void fs_callback(RF_Handle h, RF_CmdHandle ch, RF_EventMask e) //TODO delete
 //{
 //    fs_callback_called = true;
 //    if (e & RF_EventCmdDone)
@@ -61,19 +64,101 @@ static uint8_t payload_with_counter[GLOSSY_PAYLOAD_LEN_WITH_COUNT];
 //    }
 //}
 
-//void tx_callback(RF_Handle h, RF_CmdHandle ch, RF_EventMask e)
-//{
-//    tx_callback_called = true;
-//    if (e & RF_EventTxDone)
-//    {
-//      // Do something
-//    }
-//    if (e & RF_EventError)
-//    {
-//      // Do something
-//    }
-//}
-/*---------------------------------------------------------------------------*/
+void tx_callback(RF_Handle h, RF_CmdHandle ch, RF_EventMask e)
+{
+    tx_callback_called = true;
+
+    // TODO move this code under RF_EventTxDone
+    n_tx_count++;
+    if (n_tx_count == GLOSSY_N_TX) {
+      return;
+    }
+    initiator_cmd_base_time += GLOSSY_T_SLOT_WITH_ERROR;
+    RF_cmdPropTx.startTime = initiator_cmd_base_time;
+    RF_cmdPropTx.pPkt[0] = n_tx_count; // increment c (glossy relay counter)
+    //int_master_status_t int_status = int_master_read_and_disable();
+    RF_postCmd(rfHandle, (RF_Op*)&RF_cmdPropTx,
+    //                                           RF_PriorityHighest , NULL, 0);
+                                               RF_PriorityHighest , &tx_callback, RF_EventTxDone);
+    //int_master_status_set(int_status);
+
+
+    //if (e & RF_EventTxDone)
+    //{
+    //  // Do something
+    //}
+    //if (e & RF_EventError)
+    //{
+    //  // Do something
+    //}
+}
+
+void rx_callback(RF_Handle h, RF_CmdHandle ch, RF_EventMask e)
+//TODO not sure if the callback is set to the highest proprity
+{
+
+    // TODO move this code under RF_EventRxEntryDone
+    /* Extract packet and timestamp (sfd time in RAT time) from incoming data */
+    /* Get current unhandled data entry */
+    currentDataEntry = RFQueue_getDataEntry();
+
+    /* Handle the packet data, located at &currentDataEntry->data:
+     * - Length is the first byte with the current configuration
+     * - Data starts from the second byte */
+    //TODO use next line instead of defined value
+    //packetLength      = *(uint8_t*)(&currentDataEntry->data);
+    packetLength      = (uint8_t)GLOSSY_PAYLOAD_LEN_WITH_COUNT;
+    packetDataPointer = (uint8_t*)(&currentDataEntry->data + 1);
+
+    /* Copy the payload + the status byte to the packet variable */
+    memcpy(payload_with_counter, packetDataPointer, (packetLength + 1));
+    
+    memcpy(&rxTimestamp, packetDataPointer + packetLength , 4);
+
+    RFQueue_nextEntry();
+
+
+    // setup the first retransmission - next retransmisions will be handled by tx_callback
+    initiator_cmd_base_time = rxTimestamp;
+    initiator_cmd_base_time += GLOSSY_T_SLOT_WITH_ERROR;
+    RF_cmdPropTx.startTime = initiator_cmd_base_time;
+
+    // TODO not needed right !
+    RF_cmdPropTx.pktLen = GLOSSY_PAYLOAD_LEN_WITH_COUNT ;
+    RF_cmdPropTx.pPkt = payload_with_counter; //TODO this may be a mistake
+    RF_cmdPropTx.startTrigger.triggerType = TRIG_NOW; //TODO delete - test
+    
+    n_tx_count = 0;
+    RF_cmdPropTx.pPkt[0] = n_tx_count; // increment c (glossy relay counter)
+    /* start first transmission - post other tx from callbacks*/
+    //int_master_status_t int_status = int_master_read_and_disable();
+    RF_postCmd(rfHandle, (RF_Op*)&RF_cmdPropTx,
+    //                                           RF_PriorityNormal, NULL, 0);
+                                               RF_PriorityNormal, &tx_callback, RF_EventTxDone | RF_EventError | RF_EventCmdPreempted);
+    //int_master_status_set(int_status);
+
+
+    if (e & RF_EventRxEntryDone)
+    {
+      // Do something, for instance post a semaphore.
+    }
+    if (e & RF_EventNDataWritten)
+    {
+      //TODO: how to specify the number of bytes
+      // Do something
+    }
+    if (e & RF_EventRxOk)
+    {
+      //rx_callback_called = true;
+      // Do something
+    }
+    if (e & RF_EventCmdDone)
+    {
+      rx_callback_called = true;
+    }
+
+    currentTimestamp = RF_getCurrentTime();
+}
 /*---------------------------------------------------------------------------*/
 #define IS_INITIATOR() \
   (initiator_id == node_id)
@@ -84,8 +169,6 @@ glossy_start(uint16_t initiator_id, uint16_t node_id, uint8_t *payload,
 {
   static uint8_t i;
   // Packet
-  uint8_t n_tx_count;
-  uint32_t cmd_time;
 
   //static uint32_t RAT_time_first_cmd;
 
@@ -111,79 +194,128 @@ glossy_start(uint16_t initiator_id, uint16_t node_id, uint8_t *payload,
   RF_cmdPropTx.startTrigger.pastTrig = 1;
   RF_cmdPropTx.startTrigger.triggerType = TRIG_ABSTIME;
   RF_cmdPropTx.startTime = 0; //to be determined by each command later
+
+  //TODO to be deleted - just for test
+  //RF_cmdPropTX.startTrigger.pastTrig = 1;
+
   // Setting RX cmd
-  ///* Set the Data Entity queue for received data */
-  //RF_cmdPropRx.pQueue = &dataQueue;
-  ///* Discard ignored packets from Rx queue */
-  //RF_cmdPropRx.rxConf.bAutoFlushIgnored = 1;
-  ////RF_cmdPropRx.rxConf.bAppendRssi = 1;
-  //RF_cmdPropRx.rxConf.bAppendTimestamp = 1;
-  ////RF_cmdPropRx.rxConf.bIncludeHdr = 1;
-  //RF_cmdPropRx.rxConf.bAppendStatus = 0;
-  ////RF_cmdPropRX.rxConf.bAppendTimestamp = 0;
-  ///* Discard packets with CRC error from Rx queue */
-  //RF_cmdPropRx.rxConf.bAutoFlushCrcErr = 1;
-  ///* Implement packet length filtering to avoid PROP_ERROR_RXBUF */
+  if( RFQueue_defineQueue(&dataQueue,
+                          rxDataEntryBuffer,
+                          sizeof(rxDataEntryBuffer),
+                          NUM_DATA_ENTRIES,
+                          MAX_LENGTH + NUM_APPENDED_BYTES))
+  {
+      /* Failed to allocate space for all data entries */
+      while(1);
+  }
+  /* Set the Data Entity queue for received data */
+  RF_cmdPropRx.pQueue = &dataQueue;
+  /* Discard ignored packets from Rx queue */
+  RF_cmdPropRx.rxConf.bAutoFlushIgnored = 1;
+  //RF_cmdPropRx.rxConf.bAppendRssi = 1;
+  RF_cmdPropRx.rxConf.bAppendTimestamp = 1;
+  //RF_cmdPropRx.rxConf.bIncludeHdr = 1;
+  RF_cmdPropRx.rxConf.bAppendStatus = 0;
+  //RF_cmdPropRX.rxConf.bAppendTimestamp = 0;
+  /* Discard packets with CRC error from Rx queue */
+  RF_cmdPropRx.rxConf.bAutoFlushCrcErr = 1;
+  /* Implement packet length filtering to avoid PROP_ERROR_RXBUF */
   //RF_cmdPropRx.maxPktLen = MAX_LENGTH;
-  //RF_cmdPropRx.pktConf.bRepeatOk = 1;
-  //RF_cmdPropRx.pktConf.bRepeatNok = 1;
+  RF_cmdPropRx.maxPktLen = GLOSSY_PAYLOAD_LEN_WITH_COUNT;
+  RF_cmdPropRx.pktConf.bRepeatOk = 0;
+  RF_cmdPropRx.pktConf.bRepeatNok = 0;
 
   /*TODO IMPORTANT, regarding RF cmds execution time
          - first issue the commands separately but with the same time
          - if that didn't work maybe somehow i should relate the time of the commands */
 
+  // checking packets
+  //LOG_INFO("payload: ");
+  //for (i = 0; i < GLOSSY_PAYLOAD_LEN ; i++)
+  //{
+  //    printf("%u", payload[i]);
+  //}
+  //printf("\n");
+
+  //// add glossy header (the c relay counter)
+  //for(i = 0; i < (uint8_t)GLOSSY_PAYLOAD_LEN_WITH_COUNT ; i++) {
+  //  payload_with_counter[i+1] = payload[i]; //first byte reserved for header (c: the relay counter)
+  //}
+  //LOG_INFO("payload with glossy: ");
+  //for (i = 0; i < GLOSSY_PAYLOAD_LEN_WITH_COUNT  ; i++)
+  //{
+  //    printf("%u", payload_with_counter[i]);
+  //}
+  //printf("\n");
+
+  //// add glossy header (the c relay counter)
+  //for(i = 0; i < (uint8_t)GLOSSY_PAYLOAD_LEN_WITH_COUNT ; i++) {
+  //  payload_with_counter[i+1] = payload[i]; //first byte reserved for header (c: the relay counter)
+  //}
 
   if(IS_INITIATOR()) {
   // Initiator
-    // add glossy header (the c relay counter)
-    for(i = 0; i < (uint8_t)GLOSSY_PAYLOAD_LEN_WITH_COUNT ; i++) {
-      payload_with_counter[i+1] = payload[i]; //first byte reserved for header (c: the relay counter)
-    }
   
     LOG_DBG("Node is initiator, Start Tx\n");
 
     //TODO: check syncing
 
-    /* start first transmission */
-
-    cmd_time = RF_ratGetValue();
-    for(n_tx_count=0; n_tx_count<n_tx_max; n_tx_count++) {
-      /* Send packet */
-      cmd_time += GLOSSY_T_SLOT;
-      //cmd_time = GLOSSY_T_SLOT;
-      RF_cmdPropTx.startTime = cmd_time;
-      LOG_INFO("start time: %lu.\n", cmd_time);
-      //int_master_status_t int_status = int_master_read_and_disable();
-      RF_postCmd(rfHandle, (RF_Op*)&RF_cmdPropTx,
-      //                                           RF_PriorityNormal, &tx_callback, RF_EventTxDone);
-                                                 RF_PriorityNormal, NULL, 0);
-      //int_master_status_set(int_status);
-      LOG_DBG("sent packet %u.\n", n_tx_count);
-      payload_with_counter[0] +=1; // increment c (glossy relay counter)
+    // add glossy header (the c relay counter)
+    for(i = 0; i < (uint8_t)GLOSSY_PAYLOAD_LEN_WITH_COUNT ; i++) {
+      payload_with_counter[i+1] = payload[i]; //first byte reserved for header (c: the relay counter)
     }
+
+    initiator_cmd_base_time = RF_ratGetValue();
+    initiator_cmd_base_time += GLOSSY_T_SLOT_WITH_ERROR;
+    RF_cmdPropTx.startTime = initiator_cmd_base_time;
+    n_tx_count = 0;
+    RF_cmdPropTx.pPkt[0] = n_tx_count; // increment c (glossy relay counter)
+    LOG_INFO("start time of %u TX cmd: %lu.\n", n_tx_count, initiator_cmd_base_time);
+    /* start first transmission - post other tx from callbacks*/
+    //int_master_status_t int_status = int_master_read_and_disable();
+    RF_postCmd(rfHandle, (RF_Op*)&RF_cmdPropTx,
+    //                                           RF_PriorityHighest , NULL, 0);
+                                               RF_PriorityHighest , &tx_callback, RF_EventTxDone);
+    //int_master_status_set(int_status);
+
   } else {
   // Normal node not Initiator
     LOG_DBG("Node is not initiator, Start RX\n");
     ///* Glossy Receiver */
-
-    //while(!NETSTACK_RADIO.pending_packet());
-    ////TODO stamp the vht HF time
-    //LOG_DBG("Waiting for first Packet to be received\n");
-    //while(!NETSTACK_RADIO.pending_packet()) {
-    //  watchdog_periodic();
-    //}
-    //NETSTACK_RADIO.read(&payload_with_counter, GLOSSY_PAYLOAD_LEN_WITH_COUNT);
-    //LOG_DBG("A packet is received\n");
-    //  //TODO should the payload variable needs to be changed
-    ////TODO check if the packet is not corrupted, why, how !
-    //for(n_tx_count=0; n_tx_count<GLOSSY_N_TX ; n_tx_count++) {
-    //  payload_with_counter[0] +=1; // increment c (glossy relay counter)
-    //  NETSTACK_RADIO.send(&payload_with_counter, GLOSSY_PAYLOAD_LEN_WITH_COUNT);
-    //  LOG_DBG("sent packet %u.\n", n_tx_count);
-    //}
+    RF_postCmd(rfHandle, (RF_Op*)&RF_cmdPropRx,
+                                               RF_PriorityNormal, &rx_callback,
+      //RF_EventRxOk);
+      //RF_EventRxEntryDone | RF_EventNDataWritten | RF_EventRxOk);
+      RF_EventRxOk | RF_EventCmdDone);
+    // Retransmitting the packet will be handled by the callback
+    while(rx_callback_called != true) { //TODO delete
+      watchdog_periodic();
+    }
+    LOG_DBG("Received a packet.\n");
+    LOG_DBG("time of sfd: %lu\n", rxTimestamp);
+    LOG_DBG("callback time: %lu\n", currentTimestamp);
+    //LOG_DBG("current time: %lu\n", RF_getCurrentTime());
+    LOG_DBG("time of %u TX cmd should be: %lu.\n", n_tx_count, initiator_cmd_base_time);
+    uint8_t i;
+    LOG_INFO("Packet: ");
+    for (i = 0; i < GLOSSY_PAYLOAD_LEN_WITH_COUNT  ; i++)
+    {
+        printf("%u", payload_with_counter[i]);
+    }
+    printf("\n");
+    
+    while(tx_callback_called != true) { //TODO delete
+      watchdog_periodic();
+    }
+    LOG_DBG("Tx callback called.\n");
   }
   
-  //NETSTACK_RADIO.off();
+  // TODO this probably should be placed somewhere else, or even not needed at all
+  while (n_tx_count != n_tx_max) {
+    watchdog_periodic();
+    //LOG_DBG("N_TX: %u\n", n_tx_count);
+  }
+  LOG_INFO("start time of %u TX cmd: %lu.\n", n_tx_count, initiator_cmd_base_time);
 
 }
 /*---------------------------------------------------------------------------*/
--- apps/glossy_scratch/project-conf.h
+++ apps/glossy_scratch/project-conf.h
@@ -4,13 +4,16 @@
 #define LOG_LEVEL_APP LOG_LEVEL_DBG
 
 #define INITIATOR_ID    1
-#define NODE_ID    1
-#define N_TX  3
+#define NODE_ID    2
 
 #define GLOSSY_PAYLOAD_LEN 8
 #define GLOSSY_PAYLOAD_LEN_WITH_COUNT GLOSSY_PAYLOAD_LEN+1
 #define GLOSSY_N_TX                     3
-#define GLOSSY_T_SLOT                   (uint32_t) RF_convertMsToRatTicks(50) //TODO this should mean 50 ms check that
+#define GLOSSY_T_SLOT                   (uint32_t) RF_convertMsToRatTicks(30) //TODO this should mean 50 ms check that
+#define TICKS_SHIFT_ERROR               0 /* TODO the error from real tests can sometimes be 13 ticks so we should predict \
+                                                    if we need to shift this extra cycle or not  */
+#define GLOSSY_T_SLOT_WITH_ERROR        GLOSSY_T_SLOT+TICKS_SHIFT_ERROR
+//#define GLOSSY_T_SLOT_WITH_ERROR        120000
 
 
 /* Packet TX Configuration */
@@ -18,10 +21,10 @@
 #define DATA_ENTRY_HEADER_SIZE 8  /* Constant header size of a Generic Data Entry */
 #define MAX_LENGTH             30 /* Max length byte the radio will accept */
 #define NUM_DATA_ENTRIES       2  /* NOTE: Only two data entries supported at the moment */
-#define NUM_APPENDED_BYTES     5  /* The Data Entries data field will contain:
-                                   * 1 Header byte (RF_cmdPropRx.rxConf.bIncludeHdr = 0x1)
-                                   * Max 30 payload bytes
-                                   * 4 Timestamp */
+#define NUM_APPENDED_BYTES     5  // The Data Entries data field will contain: 
+                                  //  1 Header byte (RF_cmdPropRx.rxConf.bIncludeHdr = 0x1)
+                                  //  Max 30 payload bytes
+                                  //  4 Timestamp */
 
 #define VHT_LF_UPDATE_CYCLES 3 // specify after how many cycles of LF CLK does the VHT update h0
 // not all the values can be really achieved as when using any of the channels

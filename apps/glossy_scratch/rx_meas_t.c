cab2c302f85b1f7aca6d3b867b218610da11c88e
Anas Gouda <anas.ibrahim006@gmail.com>
2019-11-07 17:20:20 +0100

    one flood is working for N_TX period of 5 ms

--- apps/glossy_scratch/rx_meas_t.c
+++ apps/glossy_scratch/rx_meas_t.c
@@ -18,6 +18,7 @@
 
 /* Application Header files */
 #include "RFQueue.h"
+#include "vht.h"
 
 /* Log configuration */
 #include "sys/log.h"
@@ -61,7 +62,7 @@ static uint8_t packetLength;
 static uint8_t* packetDataPointer;
 static uint32_t rxTimestamp;
 
-//static uint32_t rxLastTimestamp = 0;
+static uint32_t rx_callback_time;
 
 static uint8_t packet[MAX_LENGTH + NUM_APPENDED_BYTES - 1]; /* The length byte is stored in a separate variable */
 
@@ -82,17 +83,11 @@ static uint8_t packet[MAX_LENGTH + NUM_APPENDED_BYTES - 1]; /* The length byte i
 static bool rx_callback_called = false;
 
 /***** Function definitions *****/
-
-/*---------------------------------------------------------------------------*/
-//void fs_callback(RF_Handle h, RF_CmdHandle ch, RF_EventMask e)
-//{
-//  fs_callback_called = true;
-//}
-
 void rx_tester_callback(RF_Handle h, RF_CmdHandle ch, RF_EventMask e)
 //TODO set the callback to the highest proprity
 {
-    if (e &  RF_EventCmdDone)
+    rx_callback_time = RF_ratGetValue();
+    if (e & RF_EventDataWritten)
     {
         /* Get current unhandled data entry */
         currentDataEntry = RFQueue_getDataEntry();
@@ -112,7 +107,7 @@ void rx_tester_callback(RF_Handle h, RF_CmdHandle ch, RF_EventMask e)
 
         //schedule RX again
         RF_postCmd(rfHandle, (RF_Op*)&RF_cmdPropRx,
-                                               RF_PriorityHighest, &rx_tester_callback, RF_EventCmdDone);
+                                               RF_PriorityHighest, &rx_tester_callback, RF_EventDataWritten);
         
         rx_callback_called = true;
     }
@@ -184,17 +179,16 @@ PROCESS_THREAD(direct_radio_process, ev, data)
     //                                           RF_PriorityNormal, &callback,
     //                                           RF_EventRxEntryDone);
     RF_postCmd(rfHandle, (RF_Op*)&RF_cmdPropRx,
-                                               RF_PriorityHighest , &rx_tester_callback, RF_EventCmdDone);
+                                               RF_PriorityHighest , &rx_tester_callback, RF_EventDataWritten);
 
     while(true) {
-      //LOG_INFO("------------------------------------------------\n");
-      //LOG_INFO("Pausing till RX received.\n");
+      LOG_INFO("------------------------------------------------\n");
+      LOG_INFO("Pausing till RX received.\n");
       while(rx_callback_called != true) {
         watchdog_periodic();
         //PROCESS_PAUSE();
       }
-      rx_callback_called = false;
-      //LOG_INFO("RX callback called.\n");
+      LOG_INFO("RX callback called.\n");
 
       uint8_t i;
       LOG_INFO("Packet: ");
@@ -203,113 +197,16 @@ PROCESS_THREAD(direct_radio_process, ev, data)
           printf("%u", packet[i]);
       }
       printf("\n");
-      //LOG_INFO("Timestamp: %lu.\n", rxTimestamp);
+      LOG_INFO("SFD Timestamp: %lu.\n", rxTimestamp);
+      LOG_INFO("cb  Timestamp: %lu.\n", rx_callback_time);
+      LOG_INFO("diff         : %lu.\n", rx_callback_time-rxTimestamp);
       //LOG_INFO("Last packet time diff >>>>>>>>>>>> : %lu.\n", rxTimestamp-rxLastTimestamp);
-      //LOG_INFO("------------------------------------------------\n");
+      LOG_INFO("------------------------------------------------\n");
       //rxLastTimestamp = rxTimestamp;
+      rx_callback_called = false;
     }
-
-    //switch(terminationReason)
-    //{
-    //    case RF_EventCmdDone:
-    //        // A radio operation command in a chain finished
-    //        break;
-    //    case RF_EventLastCmdDone:
-    //        // A stand-alone radio operation command or the last radio
-    //        // operation command in a chain finished.
-    //        break;
-    //    case RF_EventCmdCancelled:
-    //        // Command cancelled before it was started; it can be caused
-    //        // by RF_cancelCmd() or RF_flushCmd().
-    //        break;
-    //    case RF_EventCmdAborted:
-    //        // Abrupt command termination caused by RF_cancelCmd() or
-    //        // RF_flushCmd().
-    //        break;
-    //    case RF_EventCmdStopped:
-    //        // Graceful command termination caused by RF_cancelCmd() or
-    //        // RF_flushCmd().
-    //        break;
-    //    default:
-    //        // Uncaught error event
-    //        while(1);
-    //}
-
-    //uint32_t cmdStatus = ((volatile RF_Op*)&RF_cmdPropRx)->status;
-    //switch(cmdStatus)
-    //{
-    //    case PROP_DONE_OK:
-    //        // Packet received with CRC OK
-    //        break;
-    //    case PROP_DONE_RXERR:
-    //        // Packet received with CRC error
-    //        break;
-    //    case PROP_DONE_RXTIMEOUT:
-    //        // Observed end trigger while in sync search
-    //        break;
-    //    case PROP_DONE_BREAK:
-    //        // Observed end trigger while receiving packet when the command is
-    //        // configured with endType set to 1
-    //        break;
-    //    case PROP_DONE_ENDED:
-    //        // Received packet after having observed the end trigger; if the
-    //        // command is configured with endType set to 0, the end trigger
-    //        // will not terminate an ongoing reception
-    //        break;
-    //    case PROP_DONE_STOPPED:
-    //        // received CMD_STOP after command started and, if sync found,
-    //        // packet is received
-    //        break;
-    //    case PROP_DONE_ABORT:
-    //        // Received CMD_ABORT after command started
-    //        break;
-    //    case PROP_ERROR_RXBUF:
-    //        // No RX buffer large enough for the received data available at
-    //        // the start of a packet
-    //        break;
-    //    case PROP_ERROR_RXFULL:
-    //        // Out of RX buffer space during reception in a partial read
-    //        break;
-    //    case PROP_ERROR_PAR:
-    //        // Observed illegal parameter
-    //        break;
-    //    case PROP_ERROR_NO_SETUP:
-    //        // Command sent without setting up the radio in a supported
-    //        // mode using CMD_PROP_RADIO_SETUP or CMD_RADIO_SETUP
-    //        break;
-    //    case PROP_ERROR_NO_FS:
-    //        // Command sent without the synthesizer being programmed
-    //        break;
-    //    case PROP_ERROR_RXOVF:
-    //        // RX overflow observed during operation
-    //        break;
-    //    default:
-    //        // Uncaught error event - these could come from the
-    //        // pool of states defined in rf_mailbox.h
-    //        while(1);
-    //}
 
     LOG_INFO("RX Process Finished.\n");
 
     PROCESS_END();
 }
-
-//void callback(RF_Handle h, RF_CmdHandle ch, RF_EventMask e)
-//{
-//    if (e & RF_EventRxEntryDone)
-//    {
-//        /* Get current unhandled data entry */
-//        currentDataEntry = RFQueue_getDataEntry();
-//
-//        /* Handle the packet data, located at &currentDataEntry->data:
-//         * - Length is the first byte with the current configuration
-//         * - Data starts from the second byte */
-//        packetLength      = *(uint8_t*)(&currentDataEntry->data);
-//        packetDataPointer = (uint8_t*)(&currentDataEntry->data + 1);
-//
-//        /* Copy the payload + the status byte to the packet variable */
-//        memcpy(packet, packetDataPointer, (packetLength + 1));
-//
-//        RFQueue_nextEntry();
-//    }
-//}

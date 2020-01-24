0a72b552db080e8016a50143576e21ec2d1f296a
Anas Gouda <anas.ibrahim006@gmail.com>
2020-01-24 14:58:26 +0100

    change MAX_LENGHT to pkt length

--- apps/glossy_protocol/project-conf.h
+++ apps/glossy_protocol/project-conf.h
@@ -5,9 +5,9 @@
 
 /* Glossy definitions */
 #define INITIATOR_ID    1
-#define NODE_ID    2
+#define NODE_ID    1
 
-#define GLOSSY_PAYLOAD_LEN 8
+#define GLOSSY_PAYLOAD_LEN 100
 #define GLOSSY_PAYLOAD_LEN_WITH_COUNT GLOSSY_PAYLOAD_LEN+1
 #define GLOSSY_N_TX                     3
 #define GLOSSY_T_SLOT                   (uint32_t) RF_convertMsToRatTicks(1) // in RAT ticks
@@ -15,11 +15,10 @@
 
 /* Packet RX Configuration */
 #define DATA_ENTRY_HEADER_SIZE 8  /* Constant header size of a Generic Data Entry */
-#define MAX_LENGTH             30 /* Max length byte the radio will accept */
+#define MAX_LENGTH             GLOSSY_PAYLOAD_LEN_WITH_COUNT  /* Max length byte the radio will accept */
 #define NUM_DATA_ENTRIES       2  /* NOTE: Only two data entries supported at the moment */
-#define NUM_APPENDED_BYTES     5  // The Data Entries data field will contain: 
+#define NUM_APPENDED_BYTES     5  // Appended bytes contain: 
                                   //  1 Header byte (RF_cmdPropRx.rxConf.bIncludeHdr = 0x1)
-                                  //  Max 30 payload bytes
                                   //  4 Timestamp */
 
 #endif /* PROJECT_CONF_H_ */

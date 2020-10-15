31d00403ffac5cf19883379c0ac09dd6754b59b9
Anas Gouda <anas.gouda@tu-dortmund.de>
2020-10-15 12:01:54 +0200

    change TSCH to CSMA in flooding experiment

--- apps/csma_flood_test/tester.c
+++ apps/csma_flood_test/tester.c
@@ -1,6 +1,6 @@
 /*
 Based on null-net example in contiki-ng examples
-  */
+*/
 
 #include "contiki.h"
 #include "net/netstack.h"
@@ -9,21 +9,19 @@ Based on null-net example in contiki-ng examples
 #include <string.h>
 #include <stdio.h> /* For printf() */
 
+#include <sys/rtimer.h>
+
 /* Log configuration */
 #include "sys/log.h"
 #define LOG_MODULE "App"
 #define LOG_LEVEL LOG_LEVEL_INFO
 
 /* Configuration */
-static linkaddr_t dest_addr =         {{ 0x00, 0x12, 0x4b, 0x00, 0x0f, 0x2a, 0x4b, 0x83 }};
-#if MAC_CONF_WITH_TSCH
-#include "net/mac/tsch/tsch.h"
-static linkaddr_t coordinator_addr =  {{ 0x00, 0x12, 0x4b, 0x00, 0x0f, 0x2a, 0x4b, 0x83 }};
-#endif /* MAC_CONF_WITH_TSCH */
+#define SEND_INTERVAL (5*CLOCK_SECOND) //TODO increase this period so there is enough time for all kits to start sending
 
 /*---------------------------------------------------------------------------*/
 /* Global variables*/
-static process_event_t start_event;
+uint32_t rx_start, rx_end;
 
 /*---------------------------------------------------------------------------*/
 PROCESS(nullnet_example_process, "NullNet unicast example");
@@ -33,34 +31,37 @@ AUTOSTART_PROCESSES(&nullnet_example_process);
 void input_callback(const void *data, uint16_t len,
   const linkaddr_t *src, const linkaddr_t *dest)
 {
-  process_post(&nullnet_example_process,start_event,NULL);
+  rx_end = RTIMER_NOW();
 }
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(nullnet_example_process, ev, data)
 {
+  static struct etimer periodic_timer;
   static unsigned count = 0;
 
   PROCESS_BEGIN();
-
-  start_event = process_alloc_event();
-
-#if MAC_CONF_WITH_TSCH
-  tsch_set_coordinator(linkaddr_cmp(&coordinator_addr, &linkaddr_node_addr));
-#endif /* MAC_CONF_WITH_TSCH */
 
   /* Initialize NullNet */
   nullnet_buf = (uint8_t *)&count;
   nullnet_len = sizeof(count);
   nullnet_set_input_callback(input_callback);
 
+  etimer_set(&periodic_timer, SEND_INTERVAL);
   while(1) {
-    PROCESS_WAIT_EVENT_UNTIL(start_event);
-    LOG_INFO("Sending %u to ", count);
-    LOG_INFO_LLADDR(&dest_addr);
-    LOG_INFO_("\n");
+    /* send a packet so all tx start sending */
+    LOG_INFO("Sending start packet %u to all (", count);
+    LOG_INFO_LLADDR(NULL);
+    LOG_INFO_(")\n");
 
-    NETSTACK_NETWORK.output(&dest_addr);
+    NETSTACK_NETWORK.output(NULL);
+    rx_start = RTIMER_NOW();
+    etimer_reset(&periodic_timer);
     count++;
+    
+    /* measure how long it took to receive all packets*/
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
+    LOG_INFO("RX duration: %lu \n", rx_end-rx_start);
+
   }
 
   PROCESS_END();
--- apps/csma_flood_test/tx_node.c
+++ apps/csma_flood_test/tx_node.c
@@ -1,6 +1,6 @@
 /*
 Based on null-net example in contiki-ng examples
-*/
+  */
 
 #include "contiki.h"
 #include "net/netstack.h"
@@ -9,19 +9,17 @@ Based on null-net example in contiki-ng examples
 #include <string.h>
 #include <stdio.h> /* For printf() */
 
-#include <sys/rtimer.h>
-
 /* Log configuration */
 #include "sys/log.h"
 #define LOG_MODULE "App"
 #define LOG_LEVEL LOG_LEVEL_INFO
 
 /* Configuration */
-#define SEND_INTERVAL (5*CLOCK_SECOND) //TODO increase this period so there is enough time for all kits to start sending
+static linkaddr_t dest_addr =         {{ 0x00, 0x12, 0x4b, 0x00, 0x0f, 0x2a, 0x4b, 0x83 }};
 
 /*---------------------------------------------------------------------------*/
 /* Global variables*/
-uint32_t rx_start, rx_end;
+static process_event_t start_event;
 
 /*---------------------------------------------------------------------------*/
 PROCESS(nullnet_example_process, "NullNet unicast example");
@@ -31,41 +29,30 @@ AUTOSTART_PROCESSES(&nullnet_example_process);
 void input_callback(const void *data, uint16_t len,
   const linkaddr_t *src, const linkaddr_t *dest)
 {
-  rx_end = RTIMER_NOW();
+  process_post(&nullnet_example_process,start_event,NULL);
 }
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(nullnet_example_process, ev, data)
 {
-  static struct etimer periodic_timer;
   static unsigned count = 0;
 
   PROCESS_BEGIN();
 
-#if MAC_CONF_WITH_TSCH
-  tsch_set_coordinator(linkaddr_cmp(&coordinator_addr, &linkaddr_node_addr));
-#endif /* MAC_CONF_WITH_TSCH */
+  start_event = process_alloc_event();
 
   /* Initialize NullNet */
   nullnet_buf = (uint8_t *)&count;
   nullnet_len = sizeof(count);
   nullnet_set_input_callback(input_callback);
 
-  etimer_set(&periodic_timer, SEND_INTERVAL);
   while(1) {
-    /* send a packet so all tx start sending */
-    LOG_INFO("Sending start packet %u to all (", count);
-    LOG_INFO_LLADDR(NULL);
-    LOG_INFO_(")\n");
+    PROCESS_WAIT_EVENT_UNTIL(start_event);
+    LOG_INFO("Sending %u to ", count);
+    LOG_INFO_LLADDR(&dest_addr);
+    LOG_INFO_("\n");
 
-    NETSTACK_NETWORK.output(NULL);
-    rx_start = RTIMER_NOW();
-    etimer_reset(&periodic_timer);
+    NETSTACK_NETWORK.output(&dest_addr);
     count++;
-    
-    /* measure how long it took to receive all packets*/
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
-    LOG_INFO("RX duration: %lu \n", rx_end-rx_start);
-
   }
 
   PROCESS_END();
